30468c28e5cfb525e9337ad364e175e24af62ab2
Rafay Kashif <kashifrafay075@gmail.com>
2023-11-25 21:34:12 -0500

    Added insert user post request

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 3001
 
 const path = require('path')
+const database = require('./database')
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
@@ -11,8 +12,23 @@ app.get('/api', (req, res) => {
     res.json({message: "Hello from server!"})
 })
 
-app.post('/', (req, res) => {
+app.post('/register', (req, res) => {
+    const userEmail = req.body.email;
+    const userFname = req.body.firstName;
+    const userLname = req.body.lastName;
+    const userPNumber = req.body.phoneNumber;
+    const userTeamName = req.body.teamName;
 
+    database.insertUser(userEmail, userFname, userLname, userPNumber, userTeamName, (err, user) => {
+        if (err) {
+            const error = new Error(err.message);
+            res.status(500).json({error: error.message});
+        }
+
+        else {
+            res.status(200).json({email: user});
+        }
+    })
 })
 
 app.listen(port, () => console.info('Listening on port', port))
